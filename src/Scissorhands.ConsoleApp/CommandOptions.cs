547ef69d916d15a160f8660b8627901973897341
Justin Yoo <justin.yoo@aliencube.com>
2015-08-21 10:08:08 +1000

    Add console app project

---
+++ src/Scissorhands.ConsoleApp/CommandOptions.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Scissorhands.ConsoleApp
+{
+    /// <summary>
+    /// This represents the entity for commandline options.
+    /// </summary>
+    public class CommandOptions
+    {
+        /// <summary>
+        /// Gets or sets the post filepath to publish.
+        /// </summary>
+        [Option('p', "post",
+            Required = false,
+            HelpText = "Filename of the post to publish. If omitted, entire posts will be processed.",
+            DefaultValue = null)]
+        public string Post { get; set; }
+
+        /// <summary>
+        /// Gets or sets the theme name to apply.
+        /// </summary>
+        [Option('t', "theme",
+            Required = false,
+            HelpText = "Theme for post to apply",
+            DefaultValue = "default")]
+        public string Theme { get; set; }
+
+        /// <summary>
+        /// Gets the usage from the helper text of each option.
+        /// </summary>
+        /// <returns>
+        /// Returns the usage from the helper text of each option.
+        /// </returns>
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ src/Scissorhands.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using CommandLine;
+
+namespace Scissorhands.ConsoleApp
+{
+    /// <summary>
+    /// This represents the entity as the main entry point of the console app.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Process the console app from here.
+        /// </summary>
+        /// <param name="args">
+        /// The list of arguments from the commandline.
+        /// </param>
+        internal static void Main(string[] args)
+        {
+            var options = new CommandOptions();
+            if (!Parser.Default.ParseArguments(args, options))
+            {
+                Console.WriteLine("[ERROR]: failed parsing command line options.");
+                return;
+            }
+        }
+    }
+}
