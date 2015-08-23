1844620d1e69febe9aa21a4633f8907fc62d3a71
Justin Yoo <justin.yoo@aliencube.com>
2015-08-23 22:03:06 +1000

    Add test methods for YAML parser

---
+++ src/Scissorhands.Tests/Models/Settings.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Aliencube.Scissorhands.Tests.Models
+{
+    public class Settings
+    {
+        public Directories Directories { get; set; }
+
+        public List<Theme> Themes { get; set; }
+
+        public Contents Contents { get; set; }
+    }
+
+    public class Directories
+    {
+        public string Themes { get; set; }
+
+        public string Posts { get; set; }
+
+        public string Published { get; set; }
+    }
+
+    public class Theme
+    {
+        public string Name { get; set; }
+
+        public string Master { get; set; }
+
+        public string Css { get; set; }
+
+        public string Js { get; set; }
+
+        public string Images { get; set; }
+
+        public string Includes { get; set; }
+    }
+
+    public class Contents
+    {
+        public string Theme { get; set; }
+
+        public Pages Pages { get; set; }
+
+        public string Archives { get; set; }
+
+        public string Tags { get; set; }
+    }
+
+    public class Pages
+    {
+        public int Items { get; set; }
+
+        public string Format { get; set; }
+    }
+}
---
+++ src/Scissorhands.Tests/YamlTest.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Reflection;
+
+using Aliencube.Scissorhands.Tests.Models;
+
+using NUnit.Framework;
+
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Aliencube.Scissorhands.Tests
+{
+    [TestFixture]
+    public class YamlTest
+    {
+        private const string ConfigYml = "Aliencube.Scissorhands.Tests.config.yml";
+
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(ConfigYml))
+            using (var reader = new StreamReader(stream))
+            {
+                var deserialiser = new Deserializer(namingConvention: new CamelCaseNamingConvention());
+                var settings = deserialiser.Deserialize<Settings>(reader);
+            }
+        }
+    }
+}
