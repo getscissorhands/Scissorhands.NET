1aa47ae2583b82e7af1c983f148533b9067bb829
Justin Yoo <justin.yoo@aliencube.com>
2015-12-23 10:38:20 +1100

    #15 Add Scissorhands.Services.Tests project

---
+++ test/Scissorhands.Services.Tests/MarkdownServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aliencube.Scissorhands.Services.Tests
+{
+    /// <summary>
+    /// This represents the test entity for the <see cref="MarkdownService"/> class.
+    /// </summary>
+    public class MarkdownServiceTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MarkdownServiceTest"/> class.
+        /// </summary>
+        public MarkdownServiceTest()
+        {
+        }
+    }
+}
