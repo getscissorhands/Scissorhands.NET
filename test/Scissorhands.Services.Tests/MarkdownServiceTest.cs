7f5bdb93dee46854ca4a132da7891a56bc125e35
Justin Yoo <justin.yoo@aliencube.com>
2015-12-25 02:04:01 +1100

    #15, #16 Add test methods for MarkdownService

--- test/Scissorhands.Services.Tests/MarkdownServiceTest.cs
+++ test/Scissorhands.Services.Tests/MarkdownServiceTest.cs
@@ -29,7 +29,7 @@ namespace Aliencube.Scissorhands.Services.Tests
         [Theory]
         [InlineData(null)]
         [InlineData("")]
-        public async void Given_Filepath_Should_Return_Null(string filepath)
+        public async void Given_Filepath_Should_Return_NullRead(string filepath)
         {
             var result = await this._service.ReadAsync(filepath);
             result.Should().BeNull();
@@ -44,6 +44,55 @@ namespace Aliencube.Scissorhands.Services.Tests
             var filepath = "project.json";
             var result = await this._service.ReadAsync(filepath);
             result.Should().StartWithEquivalent("{");
+        }
+
+        /// <summary>
+        /// Tests whether the given markdown returns null or not.
+        /// </summary>
+        /// <param name="markdown">Markdown string.</param>
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Given_Markdown_Should_Return_NullConverted(string markdown)
+        {
+            var parsed = this._service.Parse(markdown);
+            parsed.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Tests whether the given markdown returns parsed HTML or not.
+        /// </summary>
+        [Fact]
+        public void Given_Markdown_Should_Return_ParsedHtml()
+        {
+            var markdown = "**Hello World**";
+            var parsed = this._service.Parse(markdown);
+            parsed.Should().ContainEquivalentOf("<strong>Hello World</strong>");
+        }
+
+        /// <summary>
+        /// Tests whether the given file path returns null or not.
+        /// </summary>
+        /// <param name="filepath">Fully qualified file path.</param>
+        [Theory]
+        [InlineData(null)]
+        [InlineData("blank.md")]
+        public async void Given_Filepath_Should_Return_NullConverted(string filepath)
+        {
+            var converted = await this._service.ConvertAsync(filepath);
+            converted.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Tests whether the given markdown returns parsed HTML or not.
+        /// </summary>
+        /// <param name="filepath">Fully qualified file path.</param>
+        [Theory]
+        [InlineData("hello-world.md")]
+        public async void Given_Filepath_Should_Return_ParsedHtml(string filepath)
+        {
+            var converted = await this._service.ConvertAsync(filepath);
+            converted.Should().ContainEquivalentOf("<strong>Hello World</strong>");
         }
     }
 }
