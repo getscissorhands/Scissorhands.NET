fbe428a2cf7c22873532839e6aad31c514237d07
Justin Yoo <justin.yoo@aliencube.com>
2015-12-25 00:52:46 +1100

    Add more test methods for MarkdownService

--- test/Scissorhands.Services.Tests/MarkdownServiceTest.cs
+++ test/Scissorhands.Services.Tests/MarkdownServiceTest.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Runtime;
+﻿using Aliencube.Scissorhands.Services.Tests.Fixtures;
 
-using Aliencube.Scissorhands.Services.Tests.Fixtures;
+using FluentAssertions;
 
 using Xunit;
 
@@ -20,14 +16,34 @@ namespace Aliencube.Scissorhands.Services.Tests
         /// <summary>
         /// Initializes a new instance of the <see cref="MarkdownServiceTest"/> class.
         /// </summary>
+        /// <param name="fixture"><see cref="MarkdownServiceFixture"/> instance.</param>
         public MarkdownServiceTest(MarkdownServiceFixture fixture)
         {
             this._service = fixture.MarkdownService;
         }
 
-        [Fact]
-        public void Test()
+        /// <summary>
+        /// Tests whether the given file path returns null or not.
+        /// </summary>
+        /// <param name="filepath">Fully qualified file path.</param>
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async void Given_Filepath_Should_Return_Null(string filepath)
         {
+            var result = await this._service.ReadAsync(filepath);
+            result.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Tests whether the given file path returns content or not.
+        /// </summary>
+        [Fact]
+        public async void Given_Filepath_Should_Return_Content()
+        {
+            var filepath = "project.json";
+            var result = await this._service.ReadAsync(filepath);
+            result.Should().StartWithEquivalent("{");
         }
     }
 }
