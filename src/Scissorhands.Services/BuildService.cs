f0cdbe61f69c7af0468b5ebc33547a022bdc8322
Justin Yoo <justin.yoo@aliencube.com>
2015-12-27 17:07:34 +1100

    Add BuildService

---
+++ src/Scissorhands.Services/BuildService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Aliencube.Scissorhands.Services.Helpers;
+
+namespace Aliencube.Scissorhands.Services
+{
+    /// <summary>
+    /// This represents the service entity for build.
+    /// </summary>
+    public class BuildService
+    {
+        private readonly IFileHelper _fileHelper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildService"/> class.
+        /// </summary>
+        /// <param name="fileHelper"><see cref="IFileHelper"/> instance.</param>
+        public BuildService(IFileHelper fileHelper)
+        {
+            if (fileHelper == null)
+            {
+                throw new ArgumentNullException(nameof(fileHelper));
+            }
+
+            this._fileHelper = fileHelper;
+        }
+
+        /// <summary>
+        /// The build.
+        /// </summary>
+        public void Build()
+        {
+        }
+
+        public async Task<string> PublishMarkdown(string markdown)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> PublishPost(string markdown)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
