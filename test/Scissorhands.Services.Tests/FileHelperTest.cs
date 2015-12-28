e2cb1e5294753eed9e478f4f10f4dfd5464fdecf
Justin Yoo <justin.yoo@aliencube.com>
2015-12-28 23:25:39 +1100

    Add test methods for FileHelper

--- test/Scissorhands.Services.Tests/FileHelperTest.cs
+++ test/Scissorhands.Services.Tests/FileHelperTest.cs
@@ -1,4 +1,8 @@
-﻿using Aliencube.Scissorhands.Services.Helpers;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Aliencube.Scissorhands.Services.Helpers;
 using Aliencube.Scissorhands.Services.Tests.Fixtures;
 
 using FluentAssertions;
@@ -30,7 +34,7 @@ namespace Aliencube.Scissorhands.Services.Tests
         [Theory]
         [InlineData(null)]
         [InlineData("")]
-        public async void Given_Filepath_Should_Return_NullRead(string filepath)
+        public async void Given_Filepath_ShouldReturn_Null(string filepath)
         {
             var result = await this._helper.ReadAsync(filepath).ConfigureAwait(false);
             result.Should().BeNull();
@@ -40,11 +44,58 @@ namespace Aliencube.Scissorhands.Services.Tests
         /// Tests whether the given file path returns content or not.
         /// </summary>
         [Fact]
-        public async void Given_Filepath_Should_Return_Content()
+        public async void Given_Filepath_ShouldReturn_Content()
         {
             var filepath = "project.json";
             var result = await this._helper.ReadAsync(filepath).ConfigureAwait(false);
             result.Should().StartWithEquivalent("{");
+        }
+
+        /// <summary>
+        /// Tests whether the null file path should throw an <see cref="ArgumentNullException"/> or not.
+        /// </summary>
+        [Fact]
+        public void Given_NullFilepath_ShouldThrow_ArgumentNullException()
+        {
+            Func<Task> func = async () => { await this._helper.WriteAsync(null, string.Empty).ConfigureAwait(false); };
+            func.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Tests whether the null value should return <c>False</c> or not.
+        /// </summary>
+        /// <param name="value">Content value.</param>
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async void Given_NullContent_ShouldReturn_False(string value)
+        {
+            var result = await this._helper.WriteAsync("test.bak", value).ConfigureAwait(false);
+            result.Should().BeFalse();
+        }
+
+        /// <summary>
+        /// Tests whether the null value should return <c>True</c> or not.
+        /// </summary>
+        /// <param name="value">Content value.</param>
+        [Theory]
+        [InlineData("**Hello World**")]
+        public async void Given_Content_ShouldReturn_True(string value)
+        {
+            var filepath = "test.bak";
+            var result = await this._helper.WriteAsync(filepath, value).ConfigureAwait(false);
+            result.Should().BeTrue();
+
+            var exists = File.Exists(filepath);
+            exists.Should().BeTrue();
+
+            var content = await this._helper.ReadAsync(filepath).ConfigureAwait(false);
+            content.Should().Be(value);
+
+            if (exists)
+            {
+                File.Delete(filepath);
+            }
         }
     }
 }
