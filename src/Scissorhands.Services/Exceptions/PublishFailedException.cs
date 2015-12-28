2d5f11200d9f47fe60ce3799ee5f849b184d82a3
Justin Yoo <justin.yoo@aliencube.com>
2015-12-29 01:40:38 +1100

    Add PublishFailedException

---
+++ src/Scissorhands.Services/Exceptions/PublishFailedException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+#if DNXCORE50
+
+using ApplicationException = global::System.InvalidOperationException;
+
+#endif
+
+namespace Aliencube.Scissorhands.Services.Exceptions
+{
+    /// <summary>
+    /// This represents the exception entity thrown on publish failure.
+    /// </summary>
+    public class PublishFailedException : InvalidOperationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublishFailedException"/> class.
+        /// </summary>
+        public PublishFailedException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublishFailedException"/> class.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        public PublishFailedException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublishFailedException"/> class.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        /// <param name="innerException">Inner <see cref="Exception"/> instance.</param>
+        public PublishFailedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
