37ab4f6025a1958f9b32ba1849e2d8789188ab3d
Justin Yoo <justin.yoo@aliencube.com>
2015-12-30 23:50:24 +1100

    Remove old files that are not relevant any more

--- src/src/Scissorhands.ConsoleApp/Posts/Hello-World.md
+++
@@ -1,5 +0,0 @@
-# Hello World #
-
-Welcome to Scissorhands.NET, the markdown based static blog engine written in C#.
-
-To learn more, please visit [GetScissorhands.NET](http://getscissorhands.net).
--- src/src/Scissorhands.Services/Models/PageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aliencube.Scissorhands.Services.Models
-{
-    /// <summary>
-    /// This represents the model entity for page.
-    /// </summary>
-    public class PageModel : BasePageModel
-    {
-    }
-}
