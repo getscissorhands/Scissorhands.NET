5328c54c68fcd8be9d2e96fa3f93912b656f99b1
Justin Yoo <justin.yoo@aliencube.com>
2016-01-02 22:03:59 +1100

    Add settings related types to Scissorhands.Core

---
+++ src/Scissorhands.Core/Models/Settings/FeedType.cs
@@ -0,0 +1,23 @@
+﻿namespace Scissorhands.Models.Settings
+{
+    /// <summary>
+    /// This specifies the type of feed.
+    /// </summary>
+    public enum FeedType
+    {
+        /// <summary>
+        /// Indicates that no feed type is defined.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Indicates that RSS is the feed type.
+        /// </summary>
+        Rss = 1,
+
+        /// <summary>
+        /// Indicates that ATOM is the feed type.
+        /// </summary>
+        Atom = 2,
+    }
+}
