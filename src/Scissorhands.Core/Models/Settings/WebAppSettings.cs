6e1d29f61242433779cfb8acdaf0162ebfb5de8b
Justin Yoo <justin.yoo@aliencube.com>
2016-01-02 22:04:25 +1100

    Update WebAppSettings

--- src/Scissorhands.Core/Models/Settings/WebAppSettings.cs
+++ src/Scissorhands.Core/Models/Settings/WebAppSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace Scissorhands.Models.Settings
+﻿using System.Collections.Generic;
+
+namespace Scissorhands.Models.Settings
 {
     /// <summary>
     /// This represents the entity for web app settings.
@@ -9,6 +11,26 @@
         /// Gets or sets the server.
         /// </summary>
         public virtual ServerType Server { get; set; }
+
+        /// <summary>
+        /// Gets or sets the base URL of the website. eg) http://getscissorhands.net
+        /// </summary>
+        public string BaseUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the base path of the website. Trailing slash is optional. eg) /blog or /blog/
+        /// </summary>
+        public string BasePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of <see cref="Author"/> objects.
+        /// </summary>
+        public List<Author> Authors { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of <see cref="FeedType"/> values.
+        /// </summary>
+        public List<FeedType> FeedTypes { get; set; }
 
         /// <summary>
         /// Gets or sets the theme.
