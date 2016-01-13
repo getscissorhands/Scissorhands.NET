2fda503d03e31891609a80232b6cadd4a892c451
Justin Yoo <justin.yoo@aliencube.com>
2016-01-14 00:28:27 +1100

    Update project versioning PS script

--- run-project-versioning.ps1
+++ run-project-versioning.ps1
@@ -3,8 +3,18 @@
 $projects = Get-ChildItem .\src | ?{$_.PsIsContainer}
 
 foreach($project in $projects) {
+    # Display project name
+    $project
+
+    # Move to the project
     cd src/$project
+
+    # NPM install
     npm install
+
+    # Run node.js script
     node project-version.js
+
+    # Move back to the solution root
     cd ../../
 }
