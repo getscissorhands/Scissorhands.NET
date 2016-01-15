42ca9d74667cced9f03122c6c006b663d3a366c4
Justin Yoo <justin.yoo@aliencube.com>
2016-01-16 00:23:24 +1100

    Add properties to PostFormViewModel

--- src/Scissorhands.NET/ViewModels/Post/PostFormViewModel.cs
+++ src/Scissorhands.NET/ViewModels/Post/PostFormViewModel.cs
@@ -11,13 +11,28 @@
         public string Title { get; set; }
 
         /// <summary>
+        /// Gets or sets the slug prefix.
+        /// </summary>
+        public string SlugPrefix { get; set; }
+
+        /// <summary>
         /// Gets or sets the slug.
         /// </summary>
         public string Slug { get; set; }
 
         /// <summary>
+        /// Gets or sets the author.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
         /// Gets or sets the body.
         /// </summary>
         public string Body { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comma delimited list of tags.
+        /// </summary>
+        public string Tags { get; set; }
     }
 }
