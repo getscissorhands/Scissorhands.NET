2788885443a2b1b8816b92f2c7ff9720fc75d3bd
Justin Yoo <justin.yoo@aliencube.com>
2016-01-17 12:04:01 +1100

    Add string extension method to get base root path

---
+++ src/Scissorhands.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Scissorhands.Extensions
+{
+    /// <summary>
+    /// This represents the extention entity for string.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Gets the root path if the given base path is null or empty.
+        /// </summary>
+        /// <param name="value">Babe path value.</param>
+        /// <returns>Returns the base path or root path.</returns>
+        public static string GetRootPathIfNullOrEmpty(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return "/";
+            }
+
+            if (!value.StartsWith("/"))
+            {
+                return $"/{value}";
+            }
+
+            return value;
+        }
+    }
+}
