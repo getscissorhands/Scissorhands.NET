ae49640adbbbc0a53dce2c2d30d24478223914b9
Justin Yoo <justin.yoo@aliencube.com>
2016-01-29 03:04:39 +1100

    Update run-tests.ps1 to test each framework and architecture

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,3 +1,10 @@
+# Set the list of DNVM objects
+$dnvm1 = New-Object -TypeName PSObject -Property (@{ "Version" = "1.0.0-rc1-update1"; "Runtime" = "clr"; "Architecture" = "x86" })
+$dnvm2 = New-Object -TypeName PSObject -Property (@{ "Version" = "1.0.0-rc1-update1"; "Runtime" = "clr"; "Architecture" = "x64" })
+$dnvm3 = New-Object -TypeName PSObject -Property (@{ "Version" = "1.0.0-rc1-update1"; "Runtime" = "coreclr"; "Architecture" = "x86" })
+$dnvm4 = New-Object -TypeName PSObject -Property (@{ "Version" = "1.0.0-rc1-update1"; "Runtime" = "coreclr"; "Architecture" = "x64" })
+$dnvms = ($dnvm1, $dnvm2, $dnvm3, $dnvm4)
+
 # This PowerShel script runs all test projects.
 
 $projects = Get-ChildItem .\test | ?{$_.PsIsContainer}
@@ -6,17 +13,19 @@ $exitCode = 0
 
 foreach($project in $projects) {
     # Display project name
-    $project
+    Write-Host "`n$project`n" -ForegroundColor Green
 
     # Move to the project
     cd test/$project
 
     # Run test
-    dnx test
+    foreach($dnvm in $dnvms) {
+        dnvm run $dnvm.Version -r $dnvm.Runtime -a $dnvm.Architecture test
 
-    $LASTEXITCODE
+        Write-Host "`n"
 
-    $exitCode += $LASTEXITCODE
+        $exitCode += $LASTEXITCODE
+    }
 
     # Move back to the solution root
     cd ../../
