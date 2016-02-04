9362e39865060cb0b9910855d05cf89332ea94cb
Justin Yoo <justin.yoo@aliencube.com>
2016-02-05 00:28:55 +1100

    Update /admin/index page to display all list of posts #52

--- src/Scissorhands.NET/Controllers/AdminController.cs
+++ src/Scissorhands.NET/Controllers/AdminController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using Microsoft.AspNet.Mvc;
+
+using Scissorhands.Helpers;
+using Scissorhands.Models.Settings;
 
 namespace Scissorhands.WebApp.Controllers
 {
@@ -8,46 +15,38 @@ namespace Scissorhands.WebApp.Controllers
     [Route("admin")]
     public class AdminController : Controller
     {
+        private readonly WebAppSettings _settings;
+        private readonly IFileHelper _fileHelper;
+
+        public AdminController(WebAppSettings settings, IFileHelper fileHelper)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            this._settings = settings;
+
+            if (fileHelper == null)
+            {
+                throw new ArgumentNullException(nameof(fileHelper));
+            }
+
+            this._fileHelper = fileHelper;
+        }
+
         /// <summary>
         /// Processes /home/index.
         /// </summary>
         /// <returns>Returns the view model.</returns>
         public IActionResult Index()
         {
-            return this.View();
-        }
+            var dir = this._fileHelper.GetDirectory(this._settings.HtmlPath);
+            var files = Directory.GetFiles(dir, "*.html", SearchOption.AllDirectories)
+                                 .OrderByDescending(p => p)
+                                 .Select(p => new FileInfo(p));
 
-        /// <summary>
-        /// Processes /home/about.
-        /// </summary>
-        /// <returns>Returns the view model.</returns>
-        [Route("about")]
-        public IActionResult About()
-        {
-            this.ViewData["Message"] = "Your application description page.";
-
-            return this.View();
-        }
-
-        /// <summary>
-        /// Processes /home/contact.
-        /// </summary>
-        /// <returns>Returns the view model.</returns>
-        [Route("contact")]
-        public IActionResult Contact()
-        {
-            this.ViewData["Message"] = "Your contact page.";
-
-            return this.View();
-        }
-
-        /// <summary>
-        /// Processes /home/error.
-        /// </summary>
-        /// <returns>Returns the view model.</returns>
-        [Route("error")]
-        public IActionResult Error()
-        {
+            this.ViewBag.Files = files;
             return this.View();
         }
     }
