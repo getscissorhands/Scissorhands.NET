3bedeaa49a1e9e7a526ee01dff47fbd9d7375a2a
Justin Yoo <justin.yoojh@gmail.com>
2022-01-02 22:01:37 +0900

    Fix typo on pwsh script

--- Update-Settings.ps1
+++ Update-Settings.ps1
@@ -4,7 +4,7 @@ Param(
     $ProjectPath = "./test/Scissorhands.Core.Tests",
 
     [string]
-    [Parameter(Mandatory=$false, Position=0)]
+    [Parameter(Mandatory=$false)]
     $Filename = "testsettings.json",
 
     [switch]
@@ -33,13 +33,13 @@ if ($Help -eq $true) {
 }
 
 # Update (app|test)settings.json
-Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Updating $($ProjectPath)/$(Filename) ..."
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Updating $($ProjectPath)/$($Filename) ..."
 
-$json = Get-Content -Path "$($ProjectPath)/$(Filename)" | ConvertFrom-Json
+$json = Get-Content -Path "$($ProjectPath)/$($Filename)" | ConvertFrom-Json
 $json.projectDirectory = $json.projectDirectory -replace "~", "$($pwd.Path)"
 $json.projectDirectory = $json.projectDirectory -replace "/", "$([System.IO.Path]::DirectorySeparatorChar)"
-$json | ConvertTo-Json | Set-Content -Path "$($ProjectPath)/$(Filename)" -Force
+$json | ConvertTo-Json | Set-Content -Path "$($ProjectPath)/$($Filename)" -Force
 
-Get-Content -Path "$($ProjectPath)/$(Filename)"
+Get-Content -Path "$($ProjectPath)/$($Filename)"
 
-Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Updated $($ProjectPath)/$(Filename)"
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Updated $($ProjectPath)/$($Filename)"
